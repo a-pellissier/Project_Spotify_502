e3f49468a4573d3323b5de7731edc91ca678cdb7
Achot Mouradian <achotmour@gmail.com>
2020-12-03 16:04:45 +0100

    features file with adjusted functions for extraction and associated notebook

--- Project_Spotify_502/features.py
+++ Project_Spotify_502/features.py
@@ -22,6 +22,7 @@ from tqdm import tqdm
 import Project_Spotify_502.utils as utils
 
 
+
 def columns():
     feature_sizes = dict(chroma_stft=12, chroma_cqt=12, chroma_cens=12,
                          tonnetz=6, mfcc=20, rmse=1, zcr=1,
@@ -46,8 +47,12 @@ def compute_features(tid):
 
     features = pd.Series(index=columns(), dtype=np.float32, name=tid)
 
+    processed=0
+    error=0
+    error_id=None
+
     # Catch warnings as exceptions (audioread leaks file descriptors).
-    warnings.filterwarnings('error', module='librosa')
+    warnings.filterwarnings('ignore', module='librosa')
 
     def feature_stats(name, values):
         features[name, 'mean'] = np.mean(values, axis=1)
@@ -85,7 +90,7 @@ def compute_features(tid):
         f = librosa.feature.chroma_stft(S=stft**2, n_chroma=12)
         feature_stats('chroma_stft', f)
 
-        f = librosa.feature.rmse(S=stft)
+        f = librosa.feature.rms(S=stft)
         feature_stats('rmse', f)
 
         f = librosa.feature.spectral_centroid(S=stft)
@@ -101,11 +106,15 @@ def compute_features(tid):
         del stft
         f = librosa.feature.mfcc(S=librosa.power_to_db(mel), n_mfcc=20)
         feature_stats('mfcc', f)
+        processed+=1
 
     except Exception as e:
-        print('{}: {}'.format(tid, repr(e)))
+        print(f'error = {e}')
+        error+=1
+        error_id=tid
 
-    return features
+    return features, processed, error, error_id
+
 
 def compute_features_from_mp3(tid):
 
@@ -166,33 +175,110 @@ def compute_features_from_mp3(tid):
     return features
 
 
+def compute_features_from_filepath(name, filepath):
+
+    features = pd.Series(index=columns(), dtype=np.float32, name=name)
+
+    def feature_stats(name, values):
+        features[name, 'mean'] = np.mean(values, axis=1)
+        features[name, 'std'] = np.std(values, axis=1)
+        features[name, 'skew'] = stats.skew(values, axis=1)
+        features[name, 'kurtosis'] = stats.kurtosis(values, axis=1)
+        features[name, 'median'] = np.median(values, axis=1)
+        features[name, 'min'] = np.min(values, axis=1)
+        features[name, 'max'] = np.max(values, axis=1)
+
+    filepath = filepath
+    x, sr = librosa.load(filepath, sr=None, mono=True)  # kaiser_fast
+    f = librosa.feature.zero_crossing_rate(x, frame_length=2048, hop_length=512)
+    feature_stats('zcr', f)
+
+    cqt = np.abs(librosa.cqt(x, sr=sr, hop_length=512, bins_per_octave=12,
+                                n_bins=7*12, tuning=None))
+    assert cqt.shape[0] == 7 * 12
+    assert np.ceil(len(x)/512) <= cqt.shape[1] <= np.ceil(len(x)/512)+1
+
+    f = librosa.feature.chroma_cqt(C=cqt, n_chroma=12, n_octaves=7)
+    feature_stats('chroma_cqt', f)
+    f = librosa.feature.chroma_cens(C=cqt, n_chroma=12, n_octaves=7)
+    feature_stats('chroma_cens', f)
+    f = librosa.feature.tonnetz(chroma=f)
+    feature_stats('tonnetz', f)
+
+    del cqt
+    stft = np.abs(librosa.stft(x, n_fft=2048, hop_length=512))
+    assert stft.shape[0] == 1 + 2048 // 2
+    assert np.ceil(len(x)/512) <= stft.shape[1] <= np.ceil(len(x)/512)+1
+    del x
+
+    f = librosa.feature.chroma_stft(S=stft**2, n_chroma=12)
+    feature_stats('chroma_stft', f)
+
+    f = librosa.feature.rms(S=stft)
+    feature_stats('rmse', f)
+
+    f = librosa.feature.spectral_centroid(S=stft)
+    feature_stats('spectral_centroid', f)
+    f = librosa.feature.spectral_bandwidth(S=stft)
+    feature_stats('spectral_bandwidth', f)
+    f = librosa.feature.spectral_contrast(S=stft, n_bands=6)
+    feature_stats('spectral_contrast', f)
+    f = librosa.feature.spectral_rolloff(S=stft)
+    feature_stats('spectral_rolloff', f)
+
+    mel = librosa.feature.melspectrogram(sr=sr, S=stft**2)
+    del stft
+    f = librosa.feature.mfcc(S=librosa.power_to_db(mel), n_mfcc=20)
+    feature_stats('mfcc', f)
+
+    return features
+
+
 def main():
-    tracks = utils.load('tracks.csv')
+    tracks = utils.load('../raw_data/fma_metadata/tracks.csv')
     features = pd.DataFrame(index=tracks.index,
                             columns=columns(), dtype=np.float32)
 
     # More than usable CPUs to be CPU bound, not I/O bound. Beware memory.
     nb_workers = int(1.5 * len(os.sched_getaffinity(0)))
 
-    # Longest is ~11,000 seconds. Limit processes to avoid memory errors.
-    table = ((5000, 1), (3000, 3), (2000, 5), (1000, 10), (0, nb_workers))
-    for duration, nb_workers in table:
-        print('Working with {} processes.'.format(nb_workers))
+    tids = tracks[tracks.index >= 30000][tracks['set','subset']=='small'].index
+    # to do the other blocks of data
+    # tracks[np.logical_and(tracks.index >= 30000,tracks.index < 60000)][tracks['set','subset'] == 'small']
+    
+    #initializing counters of fails vs success
 
-        tids = tracks[tracks['track', 'duration'] >= duration].index
-        tracks.drop(tids, axis=0, inplace=True)
+    
+    # creating iterable from the function, nb of processors and list for the function to loop over
+    pool = multiprocessing.Pool(nb_workers)
+    it = pool.imap_unordered(compute_features, tids)
+    
+    error_list=[]
+    processed_count=0
+    error_count=0
 
-        pool = multiprocessing.Pool(nb_workers)
-        it = pool.imap_unordered(compute_features, tids)
-
-        for i, row in enumerate(tqdm(it, total=len(tids))):
-            features.loc[row.name] = row
-
-            if i % 1000 == 0:
-                save(features, 10)
+    # iteration
+    for row, processed, error, error_id in it:
+        features.loc[row.name] = row
+        processed_count+=processed
+        error_count+=error
+        if error_id:
+            error_list.append(error_id)
 
     save(features, 10)
-    test(features, 10)
+    print(f'''
+            ###########################
+            processed: {processed_count}
+            errors: {error_count}
+            ###########################
+            errors log below:
+            ''')
+
+    for error_id in error_list:
+        print(f'            {error_id}')
+    
+    print('            ###########################')
+
 
 def main_one(tid):
     '''
@@ -210,14 +296,13 @@ def main_one(tid):
     features.to_csv('features_new.csv', float_format='%.{}e'.format(10))
 
 
-
 def save(features, ndigits):
 
     # Should be done already, just to be sure.
     features.sort_index(axis=0, inplace=True)
     features.sort_index(axis=1, inplace=True)
 
-    features.to_csv('features.csv', float_format='%.{}e'.format(ndigits))
+    features.to_csv('features_new.csv', float_format='%.{}e'.format(ndigits))
 
 
 def test(features, ndigits):
@@ -231,4 +316,5 @@ def test(features, ndigits):
 
 
 if __name__ == "__main__":
-    main_one(2)
+
+    main()
