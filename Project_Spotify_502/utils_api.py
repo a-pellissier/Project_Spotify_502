0ebfce138a600e42bde6e4acad2180a03b6e49cd
Elise Fournel <elfournel@gmail.com>
2020-12-03 21:10:20 +0100

    addding functions to generate spectro from url

--- Project_Spotify_502/utils_api.py
+++ Project_Spotify_502/utils_api.py
@@ -2,6 +2,10 @@ import librosa
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from urllib.request import urlopen
+import librosa
+import numpy as np
+import matplotlib.image
 
 def get_own_collection_preview_urls(nb_of_tracks=5, offset=0):
     '''
@@ -20,3 +24,26 @@ def get_own_collection_preview_urls(nb_of_tracks=5, offset=0):
         song_urls.append((tid, song_url))
     
     return song_urls
+
+def generate_mp3_from_sample_url(track_id, url):
+    sample_30s = urlopen(url)
+    mp3_path = f"sample_{track_id}.mp3"
+    output = open(mp3_path, 'wb')
+    output.write(sample_30s.read())
+    return mp3_path
+
+def generate_spectrogram_url(track_id, url): 
+    #loading with librosa
+    mp3_path = generate_mp3_from_sample_url(track_id, url)
+    x, sr = librosa.load(mp3_path, sr=44100, mono=True, duration = 29.976598639455784)
+    stft = np.abs(librosa.stft(x, n_fft=2048, hop_length=512))
+    mel = librosa.feature.melspectrogram(sr=sr, S=librosa.amplitude_to_db(stft))
+    img = np.flip(mel, axis=0) # put low frequencies at the bottom in image
+    img = 255-img # invert. make black==more energy
+    image_path = f'{track_id}.png'
+    matplotlib.image.imsave(image_path, img, cmap='gray')
+    return None
+
+if __name__ == '__main__': 
+    song_urls = get_own_collection_preview_urls()
+    print(song_urls)
